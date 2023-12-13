86230db49050307210e6381544d8771461d4caf5
Liân <42004259+laincs@users.noreply.github.com>
2023-12-13 09:38:26 -0300

    Fix Visual MXP Deno

--- Assets/Scripts/View/SceneAuthHandler.cs
+++ Assets/Scripts/View/SceneAuthHandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Data.Common;
 using TMPro;
 using UnityEngine;
+using System.Security.Cryptography;
+using System.Text;
+using System;
 
 public class SceneAuthHandler : MonoBehaviour
 {
@@ -21,7 +24,7 @@ public class SceneAuthHandler : MonoBehaviour
     public void TryLogin(){
         AppManager.instance.GoLoadingScene();
         if (login_username.text != "" && login_password.text != ""){
-            DBConnection.instance.StartCoroutine(DBConnection.instance.LoginUser(login_username.text, login_password.text));
+            DBConnection.instance.StartCoroutine(DBConnection.instance.LoginUser(login_username.text, ComputeSHA256Hash(login_password.text)));
         }
 
     }
@@ -29,7 +32,28 @@ public class SceneAuthHandler : MonoBehaviour
     public void TryRegister(){
         AppManager.instance.GoLoadingScene();
         if (register_username.text != "" && register_email.text != "" && register_rut.text != "" && register_cellphone.text != "" && register_password.text != "" ){
-            DBConnection.instance.StartCoroutine(DBConnection.instance.RegisterUser(register_username.text, register_rut.text, register_email.text, register_password.text, register_cellphone.text));
+            DBConnection.instance.StartCoroutine(DBConnection.instance.RegisterUser(register_username.text, register_rut.text, register_email.text, ComputeSHA256Hash(register_password.text), register_cellphone.text));
+        }
+    }
+
+    public string ComputeSHA256Hash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            // Convierte la cadena de entrada a bytes
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            // Calcula el hash SHA-256
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+            // Convierte los primeros 10 bytes del hash a una cadena hexadecimal
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Math.Min(10, hashBytes.Length); i++)
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
